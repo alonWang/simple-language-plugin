c86f5baee07b221dcc89e8f0654cd303145f7ed8
GitHub Action <action@github.com>
2021-10-21 04:10:11 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/alonwang/simplelanguageplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.alonwang.simplelanguageplugin.services
+
+import com.github.alonwang.simplelanguageplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/alonwang/simplelanguageplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/alonwang/simplelanguageplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.alonwang.simplelanguageplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.alonwang.simplelanguageplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
